93e52d663337392695921833da158fff01bdb2ca
amjad_hz <70524513+amjadhz@users.noreply.github.com>
2025-08-20 22:29:46 +0200

    Deploy site

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <div className="container">
-        <small>© {new Date().getFullYear()} Amjad. All rights reserved.</small>
-      </div>
-    </footer>
-  );
-}
